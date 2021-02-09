3dc822c8d8518b2b08619e4456876da9bafa572d
arunkumar198857 <arunkumar198857@gmail.com>
2021-02-10 02:04:31 +0530

    Assignment done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+import User from './components/User/User';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  const[users, setUsers] = useState([]);
+  const[mail, setMail] = useState('');
+  const[userFound, setUserFound] = useState(false);
+  const[searchedUser, setSearchedUser] = useState({});
+
+  useEffect(() => {
+    Axios.get('https://reqres.in/api/users?page=1')
+    .then(res => {
+      setUsers(res.data.data);
+    })
+    .catch(error => console.log(error))
+  }, [])
+
+  const onChange = (e) => {
+    setMail(e.target.value);
+    console.log(mail);
+  }
+
+  const searchUser = (e) => {
+    e.preventDefault();
+    if(!mail.match("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")){
+      alert("PLEASE ENTER A VALID EMAIL!");
+      return;
+    }
+    else{
+      for(var i = 0; i < users.length; i++){
+        if(users[i].email === mail){
+          setUserFound(true);
+          setSearchedUser(users[i]);
+          return;
+        }
+      }
+      if(userFound === false)
+        alert("USER NOT FOUND!");
+    }
+  }
+
+  return(
+    <div className="main-container">
+      <h1 id="header">Lybrate Assignment</h1>
+      <div className="searchbar">
+        <input type="email" id="search" name="mail" onChange={onChange} placeholder="Enter user email..."/>
+        <span className="searchbar__button" onClick={searchUser}>Search</span>
+      </div>
+      {userFound && 
+        <div className="user-container">
+          <h1 className="user-container__header">Search Results</h1>
+          <div className="user-container__tiles">
+            <User user={searchedUser} />
+          </div>
+        </div>
+      }
+      <div className="user-container">
+        <h1 className="user-container__header">Users</h1>
+        <div className="user-container__tiles">
+          {
+            users.map( item => (
+              <User key={item.id} user={item} />
+            ))
+          }
+        </div>
+      </div>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/components/User/User.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './User.css'
+
+const User = ({user}) => {
+    return (
+        <div className="user-container__user">
+            <div className="user-container__img">
+                <img src={user.avatar} alt="user img" id="user-img"/>
+            </div>
+            <div className="user-container__data">
+                <p id="user-container__name">{user.first_name} <b>{user.last_name}</b></p>
+                <p id="user-container__email">{user.email}</p>
+            </div>
+        </div>
+    )
+}
+
+export default User
